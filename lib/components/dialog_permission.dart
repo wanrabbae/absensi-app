725973a2ad68028936409b6e35938ca7c9cfc644
wanrabbae <wancode.msg@gmail.com>
2023-07-30 07:22:21 +0700

    Fix dialog riwayat

--- lib/components/dialog_permission.dart
+++ lib/components/dialog_permission.dart
@@ -177,44 +177,44 @@ class DialogPresensi extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Wrap(
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          // height: 500,
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            // height: 500,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
+    return Container(
+      padding: const EdgeInsets.all(20),
+      height: 700,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 20,
+            child: Row(
               children: [
-                SizedBox(
-                  height: 20,
-                  child: Row(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: ovalCardIcon(context, FeatherIcons.x,
-                            onTaped: () => Get.back()),
-                      ),
-                    ],
-                  ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: ovalCardIcon(context, FeatherIcons.x,
+                      onTaped: () => Get.back()),
                 ),
-                const Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Riwayat",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Color.fromRGBO(51, 51, 51, 1)),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
+              ],
+            ),
+          ),
+          const Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Riwayat",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Color.fromRGBO(51, 51, 51, 1)),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              // shrinkWrap: true,
+              children: [
                 Text(
                   "Tanggal",
                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
@@ -342,12 +342,14 @@ class DialogPresensi extends HookConsumerWidget {
                             BorderSide(color: Colors.black, width: 1.5)),
                   ),
                 ),
-                Padding(padding: EdgeInsets.only(bottom: 50))
+                // const SizedBox(
+                //   height: 20,
+                // ),
               ],
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
