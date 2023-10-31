383b3c069367af40a0c57704194c97bbea7e2511
Alann Maulana <alann_it06@yahoo.co.id>
2023-10-31 20:07:04 +0700

    tile izin

--- lib/views/home/components/card_home.dart
+++ lib/views/home/components/card_home.dart
@@ -1,6 +1,6 @@
 import 'package:app/global_resource.dart';
 
-Widget dataHome(BuildContext context, s, isHadir) {
+Widget dataHome(BuildContext context, s, bool isHadir) {
   Future onRefreshing() async {
     await s.dataHome();
   }
@@ -84,52 +84,104 @@ Widget dataHome(BuildContext context, s, isHadir) {
                     width: 10,
                   ),
                   Expanded(
-                      flex: 1,
-                      child: Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                SizedBox(
-                                  width: 150,
-                                  child: Text.rich(
-                                    overflow: TextOverflow.ellipsis,
-                                    strutStyle:
-                                        const StrutStyle(fontSize: 12.0),
-                                    TextSpan(
-                                      style: const TextStyle(
-                                          color: Colors.black,
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 16),
-                                      text: isHadir
-                                          ? s.absen[index]['namaKaryawan']
-                                          : s.izin?[index]['namaKaryawan'],
+                    flex: 1,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              SizedBox(
+                                width: 150,
+                                child: Text.rich(
+                                  overflow: TextOverflow.ellipsis,
+                                  strutStyle: const StrutStyle(fontSize: 12.0),
+                                  TextSpan(
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 16,
                                     ),
+                                    text: isHadir
+                                        ? s.absen[index]['namaKaryawan']
+                                        : s.izin?[index]['namaKaryawan'],
                                   ),
                                 ),
-                                // for
-                                Text(
-                                    getTimeFromDatetime(isHadir
-                                        ? s.absen[index]["waktuCheckOut"] ??
-                                            s.absen[index]['waktuCheckIn']
-                                        : s.izin[index]['tanggalStart']),
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 11,
-                                        color: Colors.black)),
-                              ],
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 5),
-                              child: Row(
-                                children: [
-                                  GestureDetector(
+                              ),
+                              // for
+                              Text(
+                                getTimeFromDatetime(isHadir
+                                    ? s.absen[index]["waktuCheckOut"] ??
+                                        s.absen[index]['waktuCheckIn']
+                                    : s.izin[index]['tanggalStart']),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 11,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: Row(
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    isHadir
+                                        ? Get.toNamed(RouteName.absenViewMode,
+                                            arguments: {
+                                                "dataAbsen": s.absen[index]
+                                              })
+                                        // showDialog(
+                                        //     context: context,
+                                        //     builder: (ctx) => dialogGoogleMap(
+                                        //         ctx,
+                                        //         latLng: LatLng(
+                                        //             double.parse(s?.absen[index]
+                                        //                 ?["alamatLatitude"]),
+                                        //             double.parse(s?.absen[index]
+                                        //                 ?["alamatLongtitude"])),
+                                        //         updateLocation: false))
+                                        : Get.toNamed(
+                                            RouteName.absenIzinDownloaded,
+                                            arguments: s.izin[index]);
+                                  },
+                                  child: isHadir
+                                      ? Image.asset(
+                                          'assets/icons/map-pin.png',
+                                          width: 16,
+                                        )
+                                      : Row(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: [
+                                            Image.asset(
+                                              'assets/icons/file-yellow.png',
+                                              height: 16,
+                                            ),
+                                            const SizedBox(width: 5),
+                                            Text(
+                                              s.izin[index]?["ijin"],
+                                              maxLines: 1,
+                                              softWrap: true,
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 16,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                ),
+                                const SizedBox(width: 5),
+                                Expanded(
+                                  flex: 1,
+                                  child: GestureDetector(
                                     onTap: () {
                                       isHadir
                                           ? Get.toNamed(RouteName.absenViewMode,
@@ -141,103 +193,63 @@ Widget dataHome(BuildContext context, s, isHadir) {
                                           //     builder: (ctx) => dialogGoogleMap(
                                           //         ctx,
                                           //         latLng: LatLng(
-                                          //             double.parse(s?.absen[index]
+                                          //             double.parse(s
+                                          //                     ?.absen[index]
                                           //                 ?["alamatLatitude"]),
-                                          //             double.parse(s?.absen[index]
-                                          //                 ?["alamatLongtitude"])),
+                                          //             double.parse(s
+                                          //                     ?.absen[index]?[
+                                          //                 "alamatLongtitude"])),
                                           //         updateLocation: false))
                                           : Get.toNamed(
                                               RouteName.absenIzinDownloaded,
                                               arguments: s.izin[index]);
                                     },
-                                    child: isHadir
-                                        ? Image.asset(
-                                            'assets/icons/map-pin.png',
-                                            width: 16,
-                                          )
-                                        : Text(s.izin[index]?["ijin"],
-                                            maxLines: 1,
-                                            softWrap: true,
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.w500,
-                                                fontSize: 12,
-                                                color: Colors.black)),
-                                  ),
-                                  const SizedBox(
-                                    width: 5,
-                                  ),
-                                  Expanded(
-                                    flex: 1,
-                                    child: GestureDetector(
-                                      onTap: () {
-                                        isHadir
-                                            ? Get.toNamed(
-                                                RouteName.absenViewMode,
-                                                arguments: {
-                                                    "dataAbsen": s.absen[index]
-                                                  })
-                                            // showDialog(
-                                            //     context: context,
-                                            //     builder: (ctx) => dialogGoogleMap(
-                                            //         ctx,
-                                            //         latLng: LatLng(
-                                            //             double.parse(s
-                                            //                     ?.absen[index]
-                                            //                 ?["alamatLatitude"]),
-                                            //             double.parse(s
-                                            //                     ?.absen[index]?[
-                                            //                 "alamatLongtitude"])),
-                                            //         updateLocation: false))
-                                            : Get.toNamed(
-                                                RouteName.absenIzinDownloaded,
-                                                arguments: s.izin[index]);
-                                      },
-                                      child: Text(
-                                          isHadir
-                                              ? s?.absen[index]['alamatPulang'].toString()
-                                                              as String ==
-                                                          "null" &&
-                                                      s?.absen[index]['alamatLoc']
-                                                                  .toString()
-                                                              as String ==
-                                                          "null"
-                                                  ? ""
-                                                  : s?.absen[index]['alamatPulang']
-                                                                  .toString()
-                                                              as String !=
-                                                          "null"
-                                                      ? s?.absen[index]
-                                                              ['alamatPulang']
-                                                          .toString() as String
-                                                      : s?.absen[index]['alamatLoc']
-                                                          .toString() as String
-                                              : "",
-                                          maxLines: 1,
-                                          softWrap: true,
-                                          style: const TextStyle(
-                                              fontWeight: FontWeight.w500,
-                                              fontSize: 12,
-                                              color: Colors.black)),
+                                    child: Text(
+                                      isHadir
+                                          ? s?.absen[index]['alamatPulang']
+                                                              .toString()
+                                                          as String ==
+                                                      "null" &&
+                                                  s?.absen[index]['alamatLoc']
+                                                              .toString()
+                                                          as String ==
+                                                      "null"
+                                              ? ""
+                                              : s?.absen[index]['alamatPulang']
+                                                              .toString()
+                                                          as String !=
+                                                      "null"
+                                                  ? s?.absen[index]
+                                                          ['alamatPulang']
+                                                      .toString() as String
+                                                  : s?.absen[index]['alamatLoc']
+                                                      .toString() as String
+                                          : "",
+                                      maxLines: 1,
+                                      softWrap: true,
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 12,
+                                        color: Colors.black,
+                                      ),
                                     ),
                                   ),
-                                  const SizedBox(
-                                    width: 20,
+                                ),
+                                const SizedBox(width: 20),
+                                if (isHadir &&
+                                    s.absen[index]["waktuCheckOut"] == null)
+                                  const Icon(
+                                    Icons.circle,
+                                    color: colorGreenPrimary2,
+                                    size: 10,
                                   ),
-                                  isHadir
-                                      ? s.absen[index]["waktuCheckOut"] == null
-                                          ? const Icon(
-                                              Icons.circle,
-                                              color: colorGreenPrimary2,
-                                              size: 10,
-                                            )
-                                          : const Text("")
-                                      : Container()
-                                ],
-                              ),
+                              ],
                             ),
-                          ],
-                        ),
-                      ))
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
