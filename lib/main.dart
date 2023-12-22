6dc3c0475c4671321780a2c9562594e8f8943d04
alann-maulana <alann_it06@yahoo.co.id>
2023-12-22 21:28:50 +0700

    init timezone

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:app/global_resource.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  tz.initializeTimeZones();
   await initialize();
   await EasyLocalization.ensureInitialized();
   SystemChrome.setPreferredOrientations(
