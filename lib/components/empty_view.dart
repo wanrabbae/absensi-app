0c37cf50ff828289708e8715b0ca02644489e8b9
Alann Maulana <alann_it06@yahoo.co.id>
2024-01-16 21:45:54 +0700

    update empty widget

--- lib/components/empty_view.dart
+++ lib/components/empty_view.dart
@@ -21,25 +21,23 @@ class EmptyView extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            child: Image.asset(image),
-          ),
-          const SizedBox(height: 8),
+          Image.asset(image, width: 277, height: 277),
+          const SizedBox(height: 20),
           Text(
             title,
             style: const TextStyle(
               fontSize: 16,
               fontWeight: FontWeight.w700,
-              color: colorBlackPrimaryHalf,
+              color: colorBlackPrimary,
             ),
             textAlign: TextAlign.center,
           ),
-          const SizedBox(height: 8),
+          const SizedBox(height: 10),
           Text(
             subtitle,
             style: const TextStyle(
               fontSize: 16,
-              color: colorBlackPrimaryHalf,
+              color: colorBlackPrimary,
             ),
             textAlign: TextAlign.center,
           ),
